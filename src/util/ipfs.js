f251126f15c51f38811107e1c528c37603e015dd
Paul MBP <paul@client-141-23-154-66.wlan.tu-berlin.de>
2019-01-21 11:53:50 +0100

    added methods for building and decoding multihash format

--- src/util/ipfs.js
+++ src/util/ipfs.js
@@ -1,6 +1,9 @@
 const ipfsAPI = require('ipfs-http-client')
 const ipfs = ipfsAPI('0.0.0.0', '5001')
 const fileReaderPullStream = require('pull-file-reader')
+const bs58 = require('bs58')
+
+
 
 export function add(file) {
     const fileStream = fileReaderPullStream(file)
@@ -8,8 +11,23 @@ export function add(file) {
         .add(fileStream)
         .then(response => {
             console.log(response)
-            return response[0].hash
+            return buildMultihash(response[0].hash)
         }).catch((err) => {
             console.log(err)
         })
 };
+
+function buildMultihash(hash){
+    const bytes = bs58.decode(hash)
+    let versionHash = bytes.toString('hex').substring(0, 4)
+    let contentHash = bytes.toString('hex').slice(4)
+    decodeMultiHash(versionHash, contentHash)
+    return [versionHash, contentHash]
+}
+
+function decodeMultiHash(versionHash, contentHash){
+    concatedBytes = Buffer.from(versionHash + contentHash, 'hex')
+    let hash = bs58.encode(concatedBytes)
+    console.log(hash)
+    return hash
+}
